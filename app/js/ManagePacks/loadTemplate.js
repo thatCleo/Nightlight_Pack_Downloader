20e2244ef05e2816595089b3defc67d93178d34a
thisMicki <hamacher.mikko@gmx.de>
2024-05-06 00:19:53 +0200

    changed manage packs page layout

--- app/js/ManagePacks/loadTemplate.js
+++ app/js/ManagePacks/loadTemplate.js
@@ -10,20 +10,28 @@ const manage_packs_template = `
     </main>
     <div id="alert_bar_container"></div>
     <div class="container px-4">
-    <div id="manage_packs_main_content" class="row row-cols-1 row-cols-lg-2 row-cols-xxl-3 g-3"></div>
-    <div class="pack-order-container-outer" id="pack-order-container-outer"></div>
-    </div><footer class="footer pb-3 nl-footer">
-    <div class="container px-4">
-        <div class="row">
-            <div class="col-xs-4 col-md-4">
-                <h3>Night<span class="text-nl">Light</span> <small class="text-muted fs-5">Icon Packs</small></h3>
-                <p class="text-muted mb-2">Icon Pack Tool. Built on top of <a class="open_link" value="https://nightlight.gg/">Nightlight.gg</a> by <a class="open_link" value="https://boop.pro/">BritishBoop</a></p>
-                <p class="text-muted"><small>Not affiliated with Behaviour Interactive, Dead by Daylight™ or Nightlight.gg in any way</small></p>
-            </div>
-        </div>
+        <div id="manage_packs_main_content" class="row row-cols-1 row-cols-lg-2 row-cols-xxl-3 g-3"></div>
     </div>
-</footer>
-</div>`;
+    <main id="manage_packs_main" class="container px-4">
+        <div>
+            <h1 class="mb-2">Manage Pack Priority</h1>
+            <div class="text-muted mb-3">Manage wich active pack takes priority over others.</div>
+        </div>
+    </main>
+    <div class="pack-order-container-outer" id="pack-order-container-outer"></div>
+    </div>
+        <footer class="footer pb-3 nl-footer">
+            <div class="container px-4">
+                <div class="row">
+                    <div class="col-xs-4 col-md-4">
+                        <h3>Night<span class="text-nl">Light</span> <small class="text-muted fs-5">Icon Packs</small></h3>
+                        <p class="text-muted mb-2">Icon Pack Tool. Built on top of <a class="open_link" value="https://nightlight.gg/">Nightlight.gg</a> by <a class="open_link" value="https://boop.pro/">BritishBoop</a></p>
+                        <p class="text-muted"><small>Not affiliated with Behaviour Interactive, Dead by Daylight™ or Nightlight.gg in any way</small></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>`;
 
 manage_packs_container.innerHTML = manage_packs_template;
 
