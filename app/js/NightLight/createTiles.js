24877c9052e8cc6453aad4bf672b8c43c461c916
thisMicki <hamacher.mikko@gmx.de>
2024-05-25 12:36:45 +0200

    download numbers are now formatted

--- app/js/NightLight/createTiles.js
+++ app/js/NightLight/createTiles.js
@@ -162,7 +162,7 @@ function setPackTiles(json) {
           avatar_element.style.color = `#${creator.user.colour}`;
         }
 
-        if(creator.user.is_supporter) {
+        if (creator.user.is_supporter) {
           avatar_element.innerHTML += supporter_icon;
         }
 
@@ -195,6 +195,10 @@ function setPackTiles(json) {
     } else if (count_days == 1) {
       packLastUpdated = 'Yesterday';
     }
+
+    let pack_downloads = pack.downloads.toLocaleString('en-US');
+
+
 
     let packContent = "";
     for (let i = 0; i < pack.has.length; i++) {
@@ -232,7 +236,7 @@ function setPackTiles(json) {
           <path fill="currentColor"
             d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-167l80 80c9.4 9.4 24.6 9.4 33.9 0l80-80c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-39 39V184c0-13.3-10.7-24-24-24s-24 10.7-24 24V318.1l-39-39c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9z">
           </path>
-        </svg><label class="downloads">${pack.downloads}</label></span></div>
+        </svg><label class="downloads">${pack_downloads}</label></span></div>
     <div title="${packContent}" class="_1he3xhb"><svg focusable="false" data-prefix="fas" data-icon="box-open" role="img"
         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-box-open">
         <path fill="currentColor"
