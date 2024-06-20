2090aa666bb7e7e737f2d004d469832c39485d4b
thisMicki <hamacher.mikko@gmx.de>
2024-06-20 12:07:24 +0200

    when trying to open a second instance, the first window will get focused instead

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('node:path')
 const { httpGet, downloadFile, downloadFileProgress, openLink } = require('./webFunctions');
 const { fileExists, clearCache, getCacheSize } = require('./fileFunctions');
 const { deletePack, downloadPack, updatePack, activatePack, resetAllPacks, getInstalledPacks, getActivePacks, getActivePacksInOrder, getPackMetaData } = require('./packFunctions');
 const { setDBDPath, setDBDPathFromDialog, getDBDPath, getCheckForPackUpdateOnStartup, setCheckForPackUpdateOnStartup } = require('./options');
 
+let win;
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1600,
     height: 900,
     minWidth: 680,
@@ -22,41 +24,54 @@ const createWindow = () => {
   win.loadFile('app/index.html')
 }
 
-app.whenReady().then(() => {
+const gotTheLock = app.requestSingleInstanceLock()
 
-  ipcMain.handle('webFunctions:httpGet', httpGet);
-  ipcMain.handle('webFunctions:dowloadFile', downloadFile);
-  ipcMain.handle('webFunctions:downloadFileProgress', downloadFileProgress);
-  ipcMain.handle('webFunctions:openLink', openLink);
-
-  ipcMain.handle('packFunctions:downloadPack', downloadPack);
-  ipcMain.handle('packFunctions:deletePack', deletePack);
-  ipcMain.handle('packFunctions:updatePack', updatePack);
-  ipcMain.handle('packFunctions:activatePack', activatePack);
-  ipcMain.handle('packFunctions:resetAllPacks', resetAllPacks);
-  ipcMain.handle('packFunctions:getInstalledPacks', getInstalledPacks);
-  ipcMain.handle('packFunctions:getActivePacks', getActivePacks);
-  ipcMain.handle('packFunctions:getActivePacksInOrder', getActivePacksInOrder);
-  ipcMain.handle('packFunctions:getPackMetaData', getPackMetaData);
-
-  ipcMain.handle('fileFunctions:fileExists', fileExists);
-  ipcMain.handle('fileFunctions:clearCache', clearCache);
-  ipcMain.handle('fileFunctions:getCacheSize', getCacheSize);
-
-  ipcMain.handle('options:setDBDPath', setDBDPath);
-  ipcMain.handle('options:setDBDPathFromDialog', setDBDPathFromDialog);
-  ipcMain.handle('options:getDBDPath', getDBDPath);
-  ipcMain.handle('options:getCheckForPackUpdateOnStartup', getCheckForPackUpdateOnStartup);
-  ipcMain.handle('options:setCheckForPackUpdateOnStartup', setCheckForPackUpdateOnStartup);
-
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (win) {
+      if (win.isMinimized()) win.restore()
+        win.focus()
     }
   })
-})
+
+  app.whenReady().then(() => {
+
+    ipcMain.handle('webFunctions:httpGet', httpGet);
+    ipcMain.handle('webFunctions:dowloadFile', downloadFile);
+    ipcMain.handle('webFunctions:downloadFileProgress', downloadFileProgress);
+    ipcMain.handle('webFunctions:openLink', openLink);
+
+    ipcMain.handle('packFunctions:downloadPack', downloadPack);
+    ipcMain.handle('packFunctions:deletePack', deletePack);
+    ipcMain.handle('packFunctions:updatePack', updatePack);
+    ipcMain.handle('packFunctions:activatePack', activatePack);
+    ipcMain.handle('packFunctions:resetAllPacks', resetAllPacks);
+    ipcMain.handle('packFunctions:getInstalledPacks', getInstalledPacks);
+    ipcMain.handle('packFunctions:getActivePacks', getActivePacks);
+    ipcMain.handle('packFunctions:getActivePacksInOrder', getActivePacksInOrder);
+    ipcMain.handle('packFunctions:getPackMetaData', getPackMetaData);
+
+    ipcMain.handle('fileFunctions:fileExists', fileExists);
+    ipcMain.handle('fileFunctions:clearCache', clearCache);
+    ipcMain.handle('fileFunctions:getCacheSize', getCacheSize);
+
+    ipcMain.handle('options:setDBDPath', setDBDPath);
+    ipcMain.handle('options:setDBDPathFromDialog', setDBDPathFromDialog);
+    ipcMain.handle('options:getDBDPath', getDBDPath);
+    ipcMain.handle('options:getCheckForPackUpdateOnStartup', getCheckForPackUpdateOnStartup);
+    ipcMain.handle('options:setCheckForPackUpdateOnStartup', setCheckForPackUpdateOnStartup);
+
+    createWindow();
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+}
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
